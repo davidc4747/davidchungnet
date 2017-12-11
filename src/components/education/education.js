18face9196abcefc0506e9add2a4f442bf04134f
David Chung <davidc4747@yahoo.com>
2017-12-11 11:28:09 -0800

    Placeholder Education Component

---
+++ src/components/education/education.js
@@ -0,0 +1,24 @@
+
+import React, {Component} from 'react';
+import './education.css';
+
+export default class Education extends Component {
+  render () {
+    return (
+        <section>
+            <div>
+                <img src="" alt="logo"/>
+                <h3>Udacity</h3>
+                <ul className="tag-list">
+                    <li className="tag">JavaScript</li>
+                    <li className="tag">Reactjs</li>
+                    <li className="tag">AJAX</li>
+                </ul>
+            </div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </section>
+    );
+  }
+}
