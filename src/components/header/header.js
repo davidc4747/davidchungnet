3bb2c301b7c49cf26db7c4cf6d582e6bacce8d2a
David Chung <davidc4747@yahoo.com>
2018-04-05 16:10:56 -0700

    Use css variables in old components, set Resume page as the old style

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -12,10 +12,10 @@ export default class Header extends Component {
             <div className="header__top">
               <h1 className="header__name">David Chung</h1>
               <ul className="header__social list-unstyled">
-                <li className="header__social-item"><a target="_blank" href="https://app.pluralsight.com/profile/davidc4747"><span className="icon fa fa-share-alt-square"></span></a></li>
-                <li className="header__social-item"><a target="_blank" href="https://linkedin.com/in/davidc4747/"><span className="icon fa fa-linkedin-square"></span></a></li>
-                <li className="header__social-item"><a target="_blank" href="https://github.com/davidc4747"><span className="icon fa fa-github-square"></span></a></li>
-                <li className="header__social-item"><a target="_blank" href="https://twitter.com/davidc4747"><span className="icon fa fa-twitter-square"></span></a></li>
+                {/* <li className="header__social-item"><a target="_blank" href="https://app.pluralsight.com/profile/davidc4747"><span className="icon fa fa-share-alt-square"></span></a></li> */}
+                {/* <li className="header__social-item"><a target="_blank" href="https://linkedin.com/in/davidc4747/"><span className="icon fab fa-linkedin-square"></span></a></li> */}
+                {/* <li className="header__social-item"><a target="_blank" href="https://github.com/davidc4747"><span className="icon fab fa-github-square"></span></a></li> */}
+                {/* <li className="header__social-item"><a target="_blank" href="https://twitter.com/davidc4747"><span className="icon fab fa-twitter-square"></span></a></li> */}
                 {/* <li className="header__social-item"><a target="_blank" href="https://codepen.io/davidc4747/"><span className="icon fa fa-square"></span></a></li> */}
               </ul>
             </div>
--- src/pages/Resume/resume.js
+++ src/pages/Resume/resume.js
@@ -1,6 +1,15 @@
 import React from 'react';
 
-export default () => {
-    return (<div>up we're here</div>)
-}
+import Header from '../../components/header/header.js';
+import Workexp from '../../components/workexp/workexp.js';
+import Portfolio from '../../components/portfolio/portfolio.js';
+import Education from '../../components/education/education.js';
 
+export default () => (
+    <div>
+        <Header />
+        <Workexp />
+        <Portfolio />
+        <Education />
+    </div>
+);
