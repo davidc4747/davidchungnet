7d0278eb91f377205a59b1f8a6506bcb552321a9
David Chung <davidc4747@yahoo.com>
2018-04-17 13:24:08 -0700

    Styled Journal pages

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -8,7 +8,7 @@ export default () => (
         <Link to="/"><li className="navbar__item"><i className="navbar__icon fa fa-home"></i> Home</li></Link>
         <Link to="/resume/"><li className="navbar__item"><i className="navbar__icon fa fa-briefcase"></i> Resume</li></Link>
         <Link to="/projects/"><li className="navbar__item"><i className="navbar__icon fa fa-code"></i> Projects</li></Link>
-        <Link to="/journal/"><li className="navbar__item"><i className="navbar__icon fa fa-book"></i> Journal</li></Link>
+        <Link to="/journal/"><li className="navbar__item"><i className="navbar__icon fa fa-book"></i> Thoughts</li></Link>
         <Link to="/faq/"><li className="navbar__item"><i className="navbar__icon fa fa-compass"></i> FAQ</li></Link>
     </ul>
 );
--- src/pages/journal/index.js
+++ src/pages/journal/index.js
@@ -1,12 +1,35 @@
 import React from 'react';
+import './journal.css';
+
 import Navbar from '../../components/navbar/navbar.js';
 
 
 
-export default () => (
+export default ({ data }) => (
     <div>
         <Navbar />
-        <h1 className="soon">Journal</h1>
-        <div className="soon">Comming Soon!!!!</div>
+        <h1 className="page-header">Thoughts</h1>
+        <ul className="journal-list">
+            {data.allJournalJson.edges.map(({ node: journal }, index) => (
+                <li key={journal.title} className="journal-item">
+                    <h2 className="journal-item__title">{index === 0 && <img className="icon" src="/imgs/Naruto.png" alt="" width="32px" />}{journal.title}</h2>
+                    <p className="journal-item__body" dangerouslySetInnerHTML={{ "__html": journal.body }}></p>
+                </li>
+            ))}
+        </ul>
     </div>
 );
+
+
+export const query = graphql`
+query journalQuery {
+    allJournalJson {
+      edges {
+        node {
+          title
+          body
+        }
+      }
+    }
+  }
+`;
