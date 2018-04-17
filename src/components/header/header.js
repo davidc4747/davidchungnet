a4257270da10a50678fb5d00989d025f8ff643b9
David Chung <davidc4747@yahoo.com>
2018-04-17 15:22:39 -0700

    Tweak header styles

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,16 +5,16 @@ import './header.css';
 export default () => (
     <header className="header">
 
-        <img className="header__headshot" src="/imgs/AniDave.jpg" alt="" />
+        <img className="header__headshot" src="/imgs/AniDave.jpg" alt="Image of David Chung" />
 
         <div className="header__top">
             <h1 className="header__name">David Chung</h1>
             <ul className="header__social list-unstyled">
-                {/* <li className="header__social-item"><a target="_blank" href="https://app.pluralsight.com/profile/davidc4747"><span className="icon fa fa-share-alt-square"></span></a></li> */}
-                {/* <li className="header__social-item"><a target="_blank" href="https://linkedin.com/in/davidc4747/"><span className="icon fab fa-linkedin-square"></span></a></li> */}
-                {/* <li className="header__social-item"><a target="_blank" href="https://github.com/davidc4747"><span className="icon fab fa-github-square"></span></a></li> */}
-                {/* <li className="header__social-item"><a target="_blank" href="https://twitter.com/davidc4747"><span className="icon fab fa-twitter-square"></span></a></li> */}
-                {/* <li className="header__social-item"><a target="_blank" href="https://codepen.io/davidc4747/"><span className="icon fa fa-square"></span></a></li> */}
+                <li className="icon header__social-item"><a target="_blank" rel="nofollow" href="https://twitter.com/davidc4747"><i className="fab fa-twitter"></i></a></li>
+                <li className="icon header__social-item"><a target="_blank" rel="nofollow" href="https://www.linkedin.com/in/davidc4747/"><i className="fab fa-linkedin-in"></i></a></li>
+                <li className="icon header__social-item"><a target="_blank" rel="nofollow" href="https://github.com/davidc4747"><i className="fab fa-github"></i></a></li>
+                <li className="icon header__social-item"><a target="_blank" rel="nofollow" href="https://codepen.io/davidc4747/"><i className="fab fa-codepen"></i></a></li>
+                <li className="icon header__social-item"><a target="_blank" rel="nofollow" href="https://www.youtube.com/user/MeNumber47/featured"><i className="fab fa-youtube"></i></a></li>
             </ul>
         </div>
 
