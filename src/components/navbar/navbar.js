965ee35f2a1166cc8e401e01016b723865c938ba
David Chung <davidc4747@yahoo.com>
2018-11-14 12:22:08 -0800

    Remove Journal

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import Link from 'gatsby-link';
-import './navbar.css';
-
+import React from 'react'
+import Link from 'gatsby-link'
+import './navbar.css'
 
 export default () => (
-    <ul className="navbar">
-        <Link to="/"><li className="navbar__item"><i className="navbar__icon fa fa-home"></i> Home</li></Link>
-        <Link to="/resume/"><li className="navbar__item"><i className="navbar__icon fa fa-briefcase"></i> Resume</li></Link>
-        <Link to="/projects/"><li className="navbar__item"><i className="navbar__icon fa fa-code"></i> Projects</li></Link>
-        <Link to="/journal/"><li className="navbar__item"><i className="navbar__icon fa fa-book"></i> Thoughts</li></Link>
-        <Link to="/faq/"><li className="navbar__item"><i className="navbar__icon fa fa-compass"></i> FAQ</li></Link>
-    </ul>
-);
+  <ul className="navbar">
+    <Link to="/">
+      <li className="navbar__item">
+        <i className="navbar__icon fa fa-home" /> Home
+      </li>
+    </Link>
+    <Link to="/resume/">
+      <li className="navbar__item">
+        <i className="navbar__icon fa fa-briefcase" /> Resume
+      </li>
+    </Link>
+    <Link to="/projects/">
+      <li className="navbar__item">
+        <i className="navbar__icon fa fa-code" /> Projects
+      </li>
+    </Link>
+    <Link to="/faq/">
+      <li className="navbar__item">
+        <i className="navbar__icon fa fa-compass" /> FAQ
+      </li>
+    </Link>
+  </ul>
+)
