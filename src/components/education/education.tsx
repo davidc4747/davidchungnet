f3d5f0992bba0e5e2e6b70e987b922222091388a
David G Chung <davidc4747@yahoo.com>
2025-04-22 20:55:23 -0700

    Education first draft

--- src/components/education/education.tsx
+++ src/components/education/education.tsx
@@ -1,6 +1,36 @@
 import { component$ } from "@builder.io/qwik";
-import "./education.css";
+import schools from "./education.json";
+import styles from "./education.module.css";
 
 export default component$(() => {
-    return <div>education</div>;
+    return (
+        <section class="page-container">
+            <div class={styles.schoolList}>
+                {schools
+                    .filter((s) => s.name != "Udacity")
+                    .map((school, index) => (
+                        <div class={styles.school} key={index}>
+                            <a href={school.url} target="_blank" rel="nofollow">
+                                <img
+                                    class={styles.logo}
+                                    src={`${school.logo}`}
+                                    alt={`${school.name} logo`}
+                                    width={100}
+                                    height={100}
+                                />
+                            </a>
+
+                            <h3 class={styles.name}>{school.name}</h3>
+                            <ul class="tag-list">
+                                {school.skills.map((skill, index) => (
+                                    <li class="tag" key={index}>
+                                        {skill}
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    ))}
+            </div>
+        </section>
+    );
 });
